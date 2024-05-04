07b96fde625cad70d3ffe9cc0415b4dee7081bb3
AshleyNDoerfler <AshleyNDoerfler@gmail.com>
2024-05-04 13:57:26 -0700

    remove unused libs, fix bug when calling lseek

--- block.c
+++ block.c
@@ -1,8 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
 #include "image.h"
+#include "block.h"
+#include <unistd.h>
 
 #define BLOCK_SIZE 4096
 
@@ -11,7 +9,7 @@ unsigned char *bread(int block_num, unsigned char *block){
     int offset = block_num * BLOCK_SIZE;
     // Seek to the block offset of the image file
     // ref: https://man7.org/linux/man-pages/man2/lseek.2.html
-    unsigned char *block = lseek(image_fd, offset, SEEK_SET);
+    lseek(image_fd, offset, SEEK_SET);
     // Read the block from the image file
     // ref: https://man7.org/linux/man-pages/man2/read.2.html
     read(image_fd, block, BLOCK_SIZE);
@@ -22,7 +20,7 @@ void bwrite(int block_num, unsigned char *block){
     // Find the block offset using block number and size
     int offset = block_num * BLOCK_SIZE;
     // Seek to the block offset of the image file
-    unsigned char *block = lseek(image_fd, offset, SEEK_SET);
+    lseek(image_fd, offset, SEEK_SET);
     // Write the block to the image file
     // ref: https://man7.org/linux/man-pages/man2/write.2.html
     write(image_fd, block, BLOCK_SIZE);
