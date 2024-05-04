d2a1898e7e24b64fef8437ba40b51e5ef35dd5fd
AshleyNDoerfler <AshleyNDoerfler@gmail.com>
2024-05-04 11:36:10 -0700

    Implement image_open method

--- image.c
+++ image.c
@@ -1,7 +1,12 @@
-
+#include "image.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
 
 int image_open(char *filename, int truncate){
-
+    // Open image file, or create if doesn't exist, truncate it to 0 if truncate true
+    return open(filename, O_RDWR | O_CREAT | (truncate ? O_TRUNC : 0), 0644);
 }
 
 int image_close(void){
