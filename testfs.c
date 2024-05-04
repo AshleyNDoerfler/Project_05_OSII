cd15a30f52619a8ba93223751235d31492645f70
AshleyNDoerfler <AshleyNDoerfler@gmail.com>
2024-05-04 14:03:06 -0700

    Add test functions and main when test enabled

--- testfs.c
+++ testfs.c
@@ -1,9 +1,43 @@
 #include "image.h"
+#include "block.h"
 #include <testfs.h>
 
-int main(){
-    image_open("testfs", 1);
-    image_close();
+#ifdef CTEST_ENABLE
 
-    return 0;
+void test_image_open(){
+    CTEST_ASSERT
 }
+
+void test_image_close(){
+    CTEST_ASSERT
+}
+
+void test_block_bread(){
+    CTEST_ASSERT
+}
+
+void test_block_bwrite(){
+    CTEST_ASSERT
+}
+
+int main(){
+    CTEST_VERBOSE(1);
+    
+    test_image_open();
+    test_image_close();
+    test_block_bread();
+    test_block_bwrite();
+
+    CTEST_RESULTS();
+    CTEST_EXIT();
+}
+
+#else
+
+int main(){
+
+    printf("Testfs is not enabled\n");
+    printf("TODO");
+}
+
+#endif
